1f1cdd7e355d48a709ebdb85152a591042324dda
Vedvart98 <yadavvedvart@gmail.com>
2026-01-19 19:43:29 +0530

    updated authRoute

--- mortgage-backend/src/routes/authRoute.js
+++ mortgage-backend/src/routes/authRoute.js
@@ -13,7 +13,7 @@ const { validateRequest } = require('../middleware/validation');
 // Register
 router.post('/register', [
   body('email').isEmail().withMessage('Please provide a valid email'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 80 characters longer'),
   body('firstName').notEmpty().withMessage('First name is required'),
   body('lastName').notEmpty().withMessage('Last name is required'),
   validateRequest
